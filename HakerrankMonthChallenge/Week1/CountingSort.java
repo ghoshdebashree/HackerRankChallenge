c71f63415b2c52466efddffc68c87de3caa820db
ghoshdebashree <shree19ghosh@gmail.com>
2021-09-16 08:04:45 +0530

    Hackerrank week 1 challenge

---
+++ HakerrankMonthChallenge/Week1/CountingSort.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import java.util.Collections.*;
+
+class Result {
+    public static List<Integer> countingSort(List<Integer> arr) {
+    int[] result = new int[arr.size()];
+    
+    for(int i =0; i< arr.size(); i++){ 
+        result[arr.get(i)]++;
+    }
+    List<Integer> list2 = new ArrayList<>();
+       for(Integer list: result) {
+         list2.add(list);
+        }
+       return list2;
+    }
+
+}
+
+public class CountingSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
